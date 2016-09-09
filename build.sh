909d2a6bfa24d0685b3c514def9fa13be5f0e6af
Oliver Trosien <otrosien@epages.com>
2016-09-09 09:17:41 +0200

    travis

--- build.sh
+++ build.sh
@@ -4,39 +4,39 @@ echo
 echo "#############################################"
 echo "# building 'shared'                         #"
 echo "#############################################"
-./shared/gradlew -p shared clean install
+./shared/gradlew -p shared clean pTML
 
 echo
 echo "#############################################"
 echo "# building 'catalog'                        #"
 echo "#############################################"
 ./catalog/gradlew -p catalog clean check bootRepackage
-docker-compose build --no-cache catalog
+#docker-compose build --no-cache catalog
 
 echo
 echo "#############################################"
 echo "# building 'order'                          #"
 echo "#############################################"
 ./order/gradlew -p order clean check bootRepackage
-docker-compose build --no-cache order
+#docker-compose build --no-cache order
 
 echo
 echo "#############################################"
 echo "# building 'bakery'                         #"
 echo "#############################################"
 ./bakery/gradlew -p bakery clean check bootRepackage
-docker-compose build --no-cache bakery
+#docker-compose build --no-cache bakery
 
 echo
 echo "#############################################"
 echo "# building 'delivery'                       #"
 echo "#############################################"
 ./delivery/gradlew -p delivery clean check bootRepackage
-docker-compose build --no-cache delivery
+#docker-compose build --no-cache delivery
 
 echo
 echo "#############################################"
 echo "# building 'order-ui'                       #"
 echo "#############################################"
 ./order-ui/gradlew -p order-ui clean check bootRepackage
-docker-compose build --no-cache orderui
+#docker-compose build --no-cache orderui
