303071634d680bb3d9daf7e7e39aae28515b2b51
Oliver Trosien <otrosien@epages.com>
2016-09-12 18:21:37 +0200

    remove apiProperties again.

--- order-ui/src/main/java/epizza/orderui/OrderUiApplication.java
+++ order-ui/src/main/java/epizza/orderui/OrderUiApplication.java
@@ -2,10 +2,8 @@ package epizza.orderui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApiProperties.class)
 public class OrderUiApplication {
 
     public static void main(String[] args) {
--- order-ui/src/test/java/epizza/orderui/OrderUiApplicationTests.java
+++ order-ui/src/test/java/epizza/orderui/OrderUiApplicationTests.java
@@ -1,25 +1,17 @@
 package epizza.orderui;
 
-import java.net.URI;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = OrderUiApplication.class, properties = {"api.baseUri=http://localhost:8082/"})
+@SpringBootTest(classes = OrderUiApplication.class)
 @ActiveProfiles({"test","local"})
 public class OrderUiApplicationTests {
 
-    @Autowired
-    private ApiProperties properties;
-
     @Test
-    public void default_properties_are_set() {
-        Assert.assertEquals(URI.create("http://localhost:8082/"), properties.getBaseUri());
+    public void contextLoads() {
     }
 }
