ae60d3c0b128e9d29f7dfc09b745d405bc4ded05
Jens Fischer <j.fischer@epages.com>
2016-09-12 17:26:02 +0200

    last minute tweaks

--- order-ui/src/main/java/epizza/orderui/ApiProperties.java
+++ order-ui/src/main/java/epizza/orderui/ApiProperties.java
@@ -3,20 +3,16 @@ package epizza.orderui;
 import java.net.URI;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix="api")
 public class ApiProperties {
 
-    private URI baseUri = URI.create("http://localhost:8082/");
-
-    public ApiProperties() {
-    }
+    private URI baseUri;
 
     public URI getBaseUri() {
         return baseUri;
     }
+
     public void setBaseUri(URI baseUri) {
         this.baseUri = baseUri;
     }
