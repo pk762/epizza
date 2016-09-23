8e92f309ddcb741d717c344548dfebb8c92cca61
Oliver Trosien <otrosien@epages.com>
2016-09-23 19:24:25 +0200

    Fix repository exposure on REST API

--- order/src/main/java/epizza/order/OrderRepository.java
+++ order/src/main/java/epizza/order/OrderRepository.java
@@ -3,7 +3,6 @@ package epizza.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface OrderRepository extends OrderRepositoryExtension, OrderRepositoryWithCriteraQuery, //
         JpaRepository<Order, Long>, JpaSpecificationExecutor<Order>, QueryDslPredicateExecutor<Order> {
--- order/src/main/java/epizza/order/OrderRepositoryExtension.java
+++ order/src/main/java/epizza/order/OrderRepositoryExtension.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource(exported = false)
 public interface OrderRepositoryExtension {
+    @RestResource(exported = false)
     @Query("from Order o where o.deliveryBoy is null")
     Page<Order> findByMissingDeliveryBoy(Pageable pageable);
 
+    @RestResource(exported = false)
     Page<Order> findByDeliveryBoyIsNull(Pageable pageable);
 }
