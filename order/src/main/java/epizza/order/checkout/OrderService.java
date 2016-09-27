1e75d2dae75263585233eb364b34f53101b09c6a
Jens Fischer <j.fischer@epages.com>
2016-09-27 13:26:06 +0200

    exception handling and de-lombokify

--- order/src/main/java/epizza/order/checkout/OrderService.java
+++ order/src/main/java/epizza/order/checkout/OrderService.java
@@ -4,9 +4,11 @@ import com.google.common.annotations.VisibleForTesting;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -15,7 +17,6 @@ import epizza.order.delivery.DeliveryJob;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static org.springframework.data.jpa.domain.Specifications.where;
 
 @Slf4j
@@ -28,8 +29,10 @@ public class OrderService {
 
     private final OrderEventPublisher orderEventPublisher;
 
-    public Order create(Order order) {
-        checkArgument(!order.getOrderItems().isEmpty(), "order does not have items");
+    public Order create(Order order) throws OrderItemsEmptyException {
+        if (order.getOrderItems().isEmpty()) {
+            throw new OrderItemsEmptyException("order does not have any items");
+        }
 
         order.setOrderedAt(LocalDateTime.now());
         Order savedOrder = orderRepository.saveAndFlush(order);
@@ -45,16 +48,6 @@ public class OrderService {
 
     public Optional<Order> getOrder(Long id) {
         return Optional.ofNullable(orderRepository.findOne(id));
-    }
-
-    public Order assignOrder(Order order, DeliveryJob deliveryJob) throws OrderAssignedException {
-        if (order.getDeliveryBoy() != null) {
-            throw new OrderAssignedException(String.format("Order '%d' is already assigned to '%s'", order.getId(), order.getDeliveryBoy()));
-        }
-        log.info("Assigning delivery job '{}' to order number {}", deliveryJob, order.getId());
-        order.setDeliveryBoy(deliveryJob.getDeliveryBoy());
-        order.setEstimatedTimeOfDelivery(deliveryJob.getEstimatedTimeOfDelivery());
-        return update(order);
     }
 
     public Page<Order> getAll(Pageable pageable) {
@@ -85,6 +78,30 @@ public class OrderService {
                 return orderRepository.findByDeliveryBoyIsNull(pageable);
             default:
                 throw new IllegalStateException(String.format("Unknown query implementation '%s'", implementation));
+        }
+    }
+
+    public Order assignOrder(Order order, DeliveryJob deliveryJob) throws OrderAssignedException {
+        if (order.getDeliveryBoy() != null) {
+            throw new OrderAssignedException(String.format("Order '%d' is already assigned to '%s'", order.getId(), order.getDeliveryBoy()));
+        }
+        log.info("Assigning delivery job '{}' to order number {}", deliveryJob, order.getId());
+        order.setDeliveryBoy(deliveryJob.getDeliveryBoy());
+        order.setEstimatedTimeOfDelivery(deliveryJob.getEstimatedTimeOfDelivery());
+        return update(order);
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Order Items is empty.")
+    public static class OrderItemsEmptyException extends RuntimeException {
+        public OrderItemsEmptyException(String message) {
+            super(message);
+        }
+    }
+
+    @ResponseStatus(code = HttpStatus.CONFLICT, reason = "Order already assigned.")
+    public static class OrderAssignedException extends RuntimeException {
+        public OrderAssignedException(String message) {
+            super(message);
         }
     }
 }
