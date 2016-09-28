de2489b474749bcf25093050dbc49d68a141fac7
Jens Fischer <j.fischer@epages.com>
2016-09-28 11:53:51 +0200

    preparing code

--- order/src/main/java/epizza/order/delivery/DeliveryController.java
+++ order/src/main/java/epizza/order/delivery/DeliveryController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 import epizza.order.checkout.Order;
 import epizza.order.checkout.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +23,7 @@ public class DeliveryController {
 
     // FIXME introduce validation
     @RequestMapping(path = "", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> assignDelivery(@PathVariable("id") Order order, @RequestBody DeliveryJob deliveryJob) {
+    public ResponseEntity<Void> assignDelivery(@PathVariable("id") Order order, @RequestBody @Valid DeliveryJob deliveryJob) {
         orderService.assignOrder(order, deliveryJob);
         return ResponseEntity.noContent().build();
     }
